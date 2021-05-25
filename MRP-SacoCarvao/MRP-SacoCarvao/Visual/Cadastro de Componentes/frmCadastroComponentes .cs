6f34e305ce10d6c3b9d2292f9a77115e612d1b80
Ricardo Gomes <sleiph@gmail.com>
2021-05-25 16:16:55 -0300

    arrumando o redimensionamento do form comps

--- MRP-SacoCarvao/MRP-SacoCarvao/Visual/Cadastro de Componentes/frmCadastroComponentes .cs
+++ MRP-SacoCarvao/MRP-SacoCarvao/Visual/Cadastro de Componentes/frmCadastroComponentes .cs
@@ -44,10 +44,13 @@ namespace MRP_SacoCarvao.Cadastro_de_Componentes
 
         private void frmCadastroComponentes_SizeChanged(object sender, EventArgs e)
         {
-            if (ListaComps_dgv.Width > 760)
-                ListaComps_dgv.Columns[6].Width = ListaComps_dgv.Width - 660;
-            else
-                ListaComps_dgv.Columns[6].Width = 100;
+            if (ListaComps_dgv.ColumnCount > 6)
+            {
+                if (ListaComps_dgv.Width > 760)
+                    ListaComps_dgv.Columns[6].Width = ListaComps_dgv.Width - 660;
+                else
+                    ListaComps_dgv.Columns[6].Width = 100;
+            }
         }
 
         // funcoes dos objetos
